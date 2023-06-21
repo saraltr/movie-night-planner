b140ff6e532b5f4b0bc1aace97f7696ee719b850
sara latorre <saralatorre97@gmail.com>
2023-06-21 15:51:32 +0200

    added function to search movies fom the api

--- src/js/externalServices.js
+++ src/js/externalServices.js
@@ -1,15 +1,34 @@
 export async function getMoviesByTitle(title) {
     const apiKey = "f8b853da";
     const baseURL = "https://www.omdbapi.com/";
-    
     const response = await fetch(`${baseURL}?apikey=${apiKey}&t=${title}`);
-    return response;
+    if (response.ok){
+        return response;
+    }
+    else{
+        throw new Error("Something went wrong");
+    }
+}
+export async function getMoviesBySearch(search) {
+    const apiKey = "f8b853da";
+    const baseURL = "https://www.omdbapi.com/";
+    const response = await fetch(`${baseURL}?apikey=${apiKey}&s=${search}`);
+    if (response.ok){
+        return response;
+    }
+    else{
+        throw new Error("Something went wrong");
+    }
 }
 
 export async function getMoviePosterById(imdbId) {
-    const apiKey = "f8b853da"; // Replace with your actual OMDB API key
+    const apiKey = "f8b853da";
     const posterURL = "https://img.omdbapi.com/";
-  
     const response = await fetch(`${posterURL}?apikey=${apiKey}&i=${imdbId}`);
-    return response;
-  }
+    if (response.ok){
+        return response;
+    }
+    else{
+        throw new Error("Failed to fetch movie poster");
+    }
+}
