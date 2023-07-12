a7613a2faeacaabc8e460718e29ba7cc79d735b2
sara latorre <saralatorre97@gmail.com>
2023-07-12 23:57:58 +0200

    added error message if the movie is not available

--- src/js/movieDetails.mjs
+++ src/js/movieDetails.mjs
@@ -1,43 +1,69 @@
-import { getMoviesByTitle } from "./externalServices.mjs";
+import { getMoviesByTitle, getMovies } from "./externalServices.mjs";
 import { getLocalStorage, setLocalStorage, addMovieToStorage } from "./utils.mjs";
-export async function displayMovieDetails(movieTitle, selector){
-  try{
-    const container = document.querySelector(selector);
-    const movie = await (await getMoviesByTitle(movieTitle)).json(); 
-//    console.log( movie)
-   if(!movie){
-       throw new SyntaxError("The movie you are trying to find is not available.")
-   }   
-   container.innerHTML = renderMovieDetails(movie)
-   const detailsContainer = document.querySelector(".detailsContainer");
-   detailsContainer.style.backgroundImage = `url("${movie.Poster}")`;
-   detailsContainer.style.backgroundSize = "cover";
-   detailsContainer.style.backgroundPosition = "center";
-   const favBtn = document.querySelector("#fav-Btn");
-   favBtn.addEventListener("click", () => {
-    addMovieToStorage(movie, "fav-list");
-   });
-   const watchlistBtn = document.querySelector("#watchlist-Btn");
-   watchlistBtn.addEventListener("click", () => {
-    addMovieToStorage(movie, "watch-list");
-   });
 
-   const watchPartyButton = document.querySelector(".joinBtn");
-   // redirects to the watch party page using the title of the movie
-   console.log(movie.Title);
-   watchPartyButton.addEventListener("click", () => {
-     const movieTitle = encodeURIComponent(movie.Title);
-     window.location.href = `/watch-party/index.html?movie=${movieTitle}`;
-   });
-  } catch(err) {
-      console.log(err) 
-  }
+export async function displayMovieDetails(movieTitle, selector) {
+    let container = document.querySelector(selector);
+    try {
+      let movie = await (await getMoviesByTitle(movieTitle)).json();
   
+      if (!movie) {
+        movie = await getMovies(`search/movie?query=${encodeURIComponent(movieTitle)}`);
+        if (!movie.results || movie.results.length === 0) {
+          throw new SyntaxError("The movie you are trying to find is not available.");
+        }
+        movie = movie.results[0];
+      }
+  
+      container.innerHTML = renderMovieDetails(movie);
+  
+      const detailsContainer = document.querySelector(".detailsContainer");
+      detailsContainer.style.backgroundImage = `url("${movie.Poster}")`;
+      detailsContainer.style.backgroundSize = "cover";
+      detailsContainer.style.backgroundPosition = "center";
+  
+      const favBtn = document.querySelector("#fav-Btn");
+      favBtn.addEventListener("click", () => {
+        addMovieToStorage(movie, "fav-list");
+      });
+  
+      const watchlistBtn = document.querySelector("#watchlist-Btn");
+      watchlistBtn.addEventListener("click", () => {
+        addMovieToStorage(movie, "watch-list");
+      });
+  
+      const watchPartyButton = document.querySelector(".joinBtn");
+      watchPartyButton.addEventListener("click", () => {
+        const movieTitle = encodeURIComponent(movie.Title);
+        window.location.href = `/watch-party/index.html?movie=${movieTitle}`;
+      });
+    } catch {
+        const gifImage = require("../public/images/working.gif");
+      const errorMessage = `
+        <div class="error-Mess">
+          <h1>The movie you are trying to find is not available yet</h1>
+          <p>We are still working on it!</p>
+          <img src="${gifImage}" alt="movie gif">
+          <p>Return to the <a href="../index.html">Home Page</a> and explore more movies!</p>
+        </div>
+      `;
+      container.innerHTML = errorMessage;
+    }
 }
 
 function renderMovieDetails(movie){
+    console.log(movie);
     const favBtn = require("../public/images/icons8-favorite-40.png");
     const watchListBtn = require("../public/images/bookmark.png");
+    let ratingsTemplate = "";
+
+    if (movie.Ratings.length > 0) {
+        for (let i = 0; i < movie.Ratings.length; i++) {
+            ratingsTemplate += `<p>🎬${movie.Ratings[i].Source}: ${movie.Ratings[i].Value}</p>`;
+        }
+    } else{
+        ratingsTemplate = "<p>Not enough ratings yet</p>";
+    }
+
     const template = `
     <div class="mov-inf">
     <h1>${movie.Title} (${movie.Year})</h1>
@@ -64,14 +90,11 @@ function renderMovieDetails(movie){
         <h3>Description</h3>
         <p>${movie.Plot}</p>
     </div>
-    
+
     <div class="mov-inf-rating">
-        <h3>⭐⭐Ratings⭐⭐<h3>
-        <p>🎬${movie.Ratings[0].Source}: ${movie.Ratings[0].Value}</p>
-        <p>🎬${movie.Ratings[1].Source}: ${movie.Ratings[1].Value}</p>
-        <p>🎬${movie.Ratings[2].Source}: ${movie.Ratings[2].Value}</p>
+    <h3>⭐⭐Ratings⭐⭐<h3>
+    ${ratingsTemplate}
     </div>
     `;
-    
     return template;
 }
