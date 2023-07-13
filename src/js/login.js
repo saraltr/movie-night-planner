cb06e6d9abe6e744dd396e3d5d0ab425709d1898
sara latorre <saralatorre97@gmail.com>
2023-07-14 00:23:26 +0200

    added search box to login page

--- src/js/login.js
+++ src/js/login.js
@@ -1,12 +1,27 @@
+import { createSearchBox, redirectToSearchResults } from './search.mjs';
 import { createScrollBtn } from './utils.mjs'
 
-const loginForm = document.getElementById("login-user");
-const redirectParam = "src/user/index.html" || "/";
 
-loginForm.addEventListener("submit", (e) => {
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.querySelector("#searchButton");
+    const searchInput = document.querySelector("#searchInput");
+  
+    //  event listener for when the research is sent
+    searchButton.addEventListener("click", () => {
+      const searchTerm = searchInput.value; // getting the value from the search input
+      redirectToSearchResults(searchTerm); // redirecting to the search results page with the search term
+    });
+
+    const loginForm = document.getElementById("login-user");
+    const redirectParam = "src/user/index.html" || "/";
+
+    loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
     window.location = "/user/index.html"
 
 });
+});
+
+createSearchBox()
 
 createScrollBtn();
